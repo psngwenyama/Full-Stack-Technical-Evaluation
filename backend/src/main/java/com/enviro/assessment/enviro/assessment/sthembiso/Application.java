681af9c77e8a47f59713cf7d2afbf15319a5f222
psngwenyama <sthembiso.ngwenyama@gmail.com>
2026-06-15 12:29:02 +0200

    Initial commit: Spring Boot backend and React frontend

---
+++ backend/src/main/java/com/enviro/assessment/enviro/assessment/sthembiso/Application.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.enviro.assessment.sthembiso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
